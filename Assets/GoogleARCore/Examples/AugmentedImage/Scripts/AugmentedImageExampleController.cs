2eea291aa4a8547183ff34a2ce1114cff0389e55
Altwath <Haidar.Altwat@gmail.com>
2021-11-21 20:25:02 +0300

    INS project

---
+++ Assets/GoogleARCore/Examples/AugmentedImage/Scripts/AugmentedImageExampleController.cs
@@ -0,0 +1,182 @@
+//-----------------------------------------------------------------------
+// <copyright file="AugmentedImageExampleController.cs" company="Google LLC">
+//
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace GoogleARCore.Examples.AugmentedImage
+{
+    using System.Collections.Generic;
+    using System.Runtime.InteropServices;
+    using GoogleARCore;
+    using UnityEngine;
+    using UnityEngine.UI;
+    using UnityEngine.EventSystems;
+    /// <summary>
+    /// Controller for AugmentedImage example.
+    /// </summary>
+    /// <remarks>
+    /// In this sample, we assume all images are static or moving slowly with
+    /// a large occupation of the screen. If the target is actively moving,
+    /// we recommend to check <see cref="AugmentedImage.TrackingMethod"/> and
+    /// render only when the tracking method equals to
+    /// <see cref="AugmentedImageTrackingMethod"/>.<c>FullTracking</c>.
+    /// See details in <a href="https://developers.google.com/ar/develop/c/augmented-images/">
+    /// Recognize and Augment Images</a>
+    /// </remarks>
+    public class AugmentedImageExampleController : MonoBehaviour
+    {
+
+        public GameObject destprefab;
+        public Camera FirstPersonCamera;
+        
+        /// <summary>
+        /// A prefab for visualizing an AugmentedImage.
+        /// </summary>
+        public AugmentedImageVisualizer AugmentedImageVisualizerPrefab;
+
+        /// <summary>
+        /// The overlay containing the fit to scan user guide.
+        /// </summary>
+        public GameObject FitToScanOverlay;
+
+        private Dictionary<int, AugmentedImageVisualizer> _visualizers
+            = new Dictionary<int, AugmentedImageVisualizer>();
+
+        private List<AugmentedImage> _tempAugmentedImages = new List<AugmentedImage>();
+
+        /// <summary>
+        /// The Unity Awake() method.
+        /// </summary>
+        public void Awake()
+        {
+            // Enable ARCore to target 60fps camera capture frame rate on supported devices.
+            // Note, Application.targetFrameRate is ignored when QualitySettings.vSyncCount != 0.
+            Application.targetFrameRate = 60;
+        }
+
+        /// <summary>
+        /// The Unity Update method.
+        /// </summary>
+        public void Update()
+        {
+            // Exit the app when the 'back' button is pressed.
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+            // Only allow the screen to sleep when not tracking.
+            if (Session.Status != SessionStatus.Tracking)
+            {
+                Screen.sleepTimeout = SleepTimeout.SystemSetting;
+            }
+            else
+            {
+                Screen.sleepTimeout = SleepTimeout.NeverSleep;
+            }
+
+            // Get updated augmented images for this frame.
+            Session.GetTrackables<AugmentedImage>(
+                _tempAugmentedImages, TrackableQueryFilter.Updated);
+
+            // Create visualizers and anchors for updated augmented images that are tracking and do
+            // not previously have a visualizer. Remove visualizers for stopped images.
+            foreach (var image in _tempAugmentedImages)
+            {
+                AugmentedImageVisualizer visualizer = null;
+                _visualizers.TryGetValue(image.DatabaseIndex, out visualizer);
+                if (image.TrackingState == TrackingState.Tracking && visualizer == null)
+                {
+                    // Create an anchor to ensure that ARCore keeps tracking this augmented image.
+                    Anchor anchor = image.CreateAnchor(image.CenterPose);
+                    visualizer = (AugmentedImageVisualizer)Instantiate(
+                        AugmentedImageVisualizerPrefab, anchor.transform);
+                    visualizer.Image = image;
+                    _visualizers.Add(image.DatabaseIndex, visualizer);
+                    var dest = Instantiate(destprefab, Vector3.zero , Quaternion.identity);
+                    dest.transform.parent = anchor.transform;
+
+
+                }
+                else if (image.TrackingState == TrackingState.Stopped && visualizer != null)
+                {
+                    _visualizers.Remove(image.DatabaseIndex);
+                    GameObject.Destroy(visualizer.gameObject);
+                }
+            }
+//LOGIC TO HANDLE PLANE TOUCHES
+            Touch touch;
+            if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+            {
+                //feedbackText.text = "Returning at no input";
+                  return;
+               
+            }
+
+            // Should not handle input if the player is pointing on UI.
+            if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+            {
+                //feedbackText.text = "Returninng over UI TOUCH";
+                return;
+            }
+
+
+            TrackableHit hit;
+            TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+                TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+            if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+            {
+                // Use hit pose and camera pose to check if hittest is from the
+                // back of the plane, if it is, no need to create the anchor.
+                if ((hit.Trackable is DetectedPlane) &&
+                    Vector3.Dot(FirstPersonCamera.transform.position - hit.Pose.position,
+                        hit.Pose.rotation * Vector3.up) < 0)
+                {
+                    Debug.Log("Hit at back of the current DetectedPlane");
+                }
+                else
+                {
+                    // Instantiate prefab at the hit pose.
+                            var gameObject = Instantiate(destprefab, hit.Pose.position, hit.Pose.rotation);
+
+                            // Compensate for the hitPose rotation facing away from the raycast (i.e.
+                            // camera).
+                            gameObject.transform.Rotate(-90, 0, 0, Space.Self);
+
+                            // Create an anchor to allow ARCore to track the hitpoint as understanding of
+                            // the physical world evolves.
+                            var anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                            // Make game object a child of the anchor.
+                            gameObject.transform.parent = anchor.transform;
+                }}
+            // Show the fit-to-scan overlay if there are no images that are Tracking.
+            foreach (var visualizer in _visualizers.Values)
+            {
+                if (visualizer.Image.TrackingState == TrackingState.Tracking)
+                {
+                    FitToScanOverlay.SetActive(false);
+                    return;
+                }
+            }
+
+            FitToScanOverlay.SetActive(true);
+        }
+    }
+}
